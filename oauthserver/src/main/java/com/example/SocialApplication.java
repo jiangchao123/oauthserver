661efdda72f7361d3dfbe067f17156f709746c2b
jiangchao08 <jiangchao08@baidu.com>
2017-03-04 13:10:17 +0800

    初始化项目

---
+++ oauthserver/src/main/java/com/example/SocialApplication.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+// 认证服务器
+@EnableAuthorizationServer
+public class SocialApplication {
+
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
+	}
+
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "hello word!";
+	}
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		// @formatter:off
+//		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+//				.authenticated().and().exceptionHandling()
+//				// 没授权的用户将会跳转到此页面
+//				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"));
+//				// 增加过滤链，处理要认证的链接
+//		// @formatter:on
+//	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http.antMatcher("/user").authorizeRequests().anyRequest().authenticated();
+			// @formatter:on
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+}
